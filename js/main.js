49c4513f2851f1c2f7f5b859e2efcf151ad14f90
raudbtw <105887271+raudbtw@users.noreply.github.com>
2022-09-01 15:04:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,73 @@
+const form = document.querySelector("#form");
+const taskInput = document.querySelector("#taskInput");
+const tasksList = document.querySelector("#tasksList");
+const emptyList = document.querySelector("#emptyList");
+
+form.addEventListener("submit", addTask);
+
+tasksList.addEventListener("click", deleteTask);
+
+tasksList.addEventListener("click", doneTask);
+
+let tasks = [];
+
+function addTask(event) {
+  event.preventDefault();
+  const taskText = taskInput.value;
+
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    done: false,
+  };
+
+  tasks.push(newTask);
+
+  const cssClass = newTask.done ? "task-title task-title--done" : "task-title";
+
+  const taskHTML = ` <li id = '${newTask.id}' class="list-group-item d-flex justify-content-between task-item">
+  <span class="${cssClass}">${newTask.text}</span>
+  <div class="task-item__buttons">
+    <button type="button" data-action="done" class="btn-action">
+      <img src="./img/tick.svg" alt="Done" width="18" height="18" />
+    </button>
+    <button type="button" data-action="delete" class="btn-action">
+      <img src="./img/cross.svg" alt="Done" width="18" height="18" />
+    </button>
+  </div>
+</li>`;
+  tasksList.insertAdjacentHTML("beforeend", taskHTML);
+
+  taskInput.value = "";
+  taskInput.focus();
+
+  if (tasksList.children.length > 1) {
+    emptyList.classList.add("none");
+  }
+}
+
+function deleteTask(event) {
+  if (event.target.dataset.action !== "delete") return;
+
+  const parentNode = event.target.closest(".list-group-item");
+
+  const id = parentNode.id;
+
+  const index = tasks.findIndex((task) => task.id == id);
+
+  tasks.splice(index, 1);
+
+  parentNode.remove();
+
+  if (tasksList.children.length === 1) {
+    emptyList.classList.remove("none");
+  }
+}
+
+function doneTask(event) {
+  if (event.target.dataset.action === "done") {
+    const parentNode = event.target.closest(".list-group-item");
+    const taskTitle = parentNode.querySelector(".task-title");
+    taskTitle.classList.toggle("task-title--done");
+  }
+}
